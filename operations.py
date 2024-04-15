378d257a5fee40581882add38a7907516effc454
Fabrizio Barrios Blanco <a01784901@tec.mx>
2024-04-15 16:16:29 -0600

    Creacion de operaciones suma y resta

--- operations.py
+++ operations.py
@@ -2,7 +2,9 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
-
+addition = a+b
+print("The sum of the numbers is: ", addition)
 # Display the subtraction
-
+subtraction = a-b
+print("The difference of the numbers is: ", subtraction)
 # Display multiplication
