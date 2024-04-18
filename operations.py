173714ff1f1f4f0b522daa9c2f80d72aece7c54a
Miguel Soria <a01028033@tec.mx>
2024-04-18 13:42:36 -0600

    testing commit

--- operations.py
+++ operations.py
@@ -5,9 +5,5 @@ b = int(input(“Give me the second number:”))
 addition = a+b
 print("The sum of the numbers is: ", addition)
 # Display the subtraction
-<<<<<<< HEAD
-=======
 subtraction = a-b
 print("The difference of the numbers is: ", subtraction)
-# Display multiplication
->>>>>>> 378d257a5fee40581882add38a7907516effc454
